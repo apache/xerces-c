896fd939f6628707a7d03d72b1a5ecfd2ed7dee5
Unknown (roddey) <dev-null@apache.org>
1999-11-30 20:23:42 +0000

    Fixed it to deal with recent changes to the directory structure.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@171433 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/Win32/ccd.cmd
+++ scripts/Win32/ccd.cmd
@@ -8,13 +8,13 @@ IF "%1"=="dom" GOTO CD_DOM
 IF "%1"=="framework" GOTO CD_FRAMEWORK
 IF "%1"=="internal" GOTO CD_INTERNAL
 IF "%1"=="intl" GOTO CD_INTL
-IF "%1"=="intl" GOTO CD_INTLBUILD
 IF "%1"=="parsers" GOTO CD_PARSERS
 IF "%1"=="samples" GOTO CD_SAMPLES
 IF "%1"=="sax" GOTO CD_SAX
 IF "%1"=="scripts" GOTO CD_SCRIPTS
 IF "%1"=="src" GOTO CD_SRC
 IF "%1"=="tests" GOTO CD_TESTS
+IF "%1"=="tools" GOTO CD_TOOLS
 IF "%1"=="util" GOTO CD_UTIL
 
 
@@ -30,27 +30,23 @@ CD %XML4CRoot%\samples\data
 GOTO EXIT
 
 :CD_DOM
-CD %XML4CRoot%\src\com\ibm\xml\dom
+CD %XML4CRoot%\src\dom
 GOTO EXIT
 
 :CD_FRAMEWORK
-CD %XML4CRoot%\src\com\ibm\xml\framework
+CD %XML4CRoot%\src\framework
 GOTO EXIT
 
 :CD_INTERNAL
-CD %XML4CRoot%\src\com\ibm\xml\internal
+CD %XML4CRoot%\src\internal
 GOTO EXIT
 
 :CD_INTL
 CD %XML4CRoot%\intlFiles
 GOTO EXIT
 
-:CD_INTLBUILD
-CD %XML4CRoot%\intlFiles\Build\Win32\VC6
-GOTO EXIT
-
 :CD_PARSERS
-CD %XML4CRoot%\src\com\ibm\xml\parsers
+CD %XML4CRoot%\src\parsers
 GOTO EXIT
 
 :CD_SAMPLES
@@ -70,11 +66,15 @@ CD %XML4CRoot%\src
 GOTO EXIT
 
 :CD_TESTS
-CD %XML4CRoot%\src\com\ibm\xml\util\tests
+CD %XML4CRoot%\tests
+GOTO EXIT
+
+:CD_TOOLS
+CD %XML4CRoot%\tools
 GOTO EXIT
 
 :CD_UTIL
-CD %XML4CRoot%\src\com\ibm\xml\util
+CD %XML4CRoot%\src\util
 GOTO EXIT
 
 :EXIT
