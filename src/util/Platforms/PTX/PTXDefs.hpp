87215ec921ef50201b90fe095e12eb4ba07560e1
Unknown (abagchi) <dev-null@apache.org>
2000-04-04 20:12:36 +0000

    Initial checkin
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@172006 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/util/Platforms/PTX/PTXDefs.hpp
@@ -0,0 +1,31 @@
+/*
+ * (C) Copyright IBM Corp. 1999  All rights reserved.
+ *
+ * US Government Users Restricted Rights Use, duplication or
+ * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ *
+ * The program is provided "as is" without any warranty express or
+ * implied, including the warranty of non-infringement and the implied
+ * warranties of merchantibility and fitness for a particular purpose.
+ * IBM will not be liable for any damages suffered by you as a result
+ * of using the Program. In no event will IBM be liable for any
+ * special, indirect or consequential damages or lost profits even if
+ * IBM has been advised of the possibility of their occurrence. IBM
+ * will not be liable for any third party claims against you.
+ */
+
+/**
+ *  @version Revision: 19 1.1 src/com/ibm/xml/util/PTXDefs.hpp, xml4c2Utils, xml4c2, xml4c2_3_2 
+ */
+
+
+// ---------------------------------------------------------------------------
+//  Dynix/PTX runs in little endian mode
+// ---------------------------------------------------------------------------
+#define ENDIANMODE_LITTLE
+typedef void* FileHandle;
+
+#ifndef PTX
+#define PTX
+#endif
+
