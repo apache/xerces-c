e513f409b6993580637ad999e659a1d0478c202a
Unknown (aruna1) <dev-null@apache.org>
2000-06-21 01:02:02 +0000

    Updated changes to incorporate version.incl in binary pack and remove occurences of Template files
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@172202 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/packageBinaries.pl
+++ scripts/packageBinaries.pl
@@ -252,7 +252,6 @@ if ($platform =~ m/Windows/  || $platform =~ m/CYGWIN/) {
     }
     
     
-    
     # Populate the samples directory
     print ("\n\nCopying sample files ...\n");
     system("cp -Rfv $XERCESCROOT/samples/Projects/* $targetdir/samples/Projects");
@@ -506,6 +505,8 @@ if ( ($platform =~ m/AIX/i)    || ($platform =~ m/HP-UX/i) ||
     system("cp -Rf $XERCESCROOT/src/framework/*.hpp $targetdir/include/framework");
     system("cp -Rf $XERCESCROOT/src/dom/D*.hpp $targetdir/include/dom");
     
+    system("cp -Rf $XERCESCROOT/*.incl $targetdir");
+    
     system("rm -f $targetdir/include/dom/*Impl.hpp");
     system("rm -f $targetdir/include/dom/DS*.hpp");
     system("cp -Rf $XERCESCROOT/src/internal/*.hpp $targetdir/include/internal");
@@ -581,6 +582,9 @@ if ( ($platform =~ m/AIX/i)    || ($platform =~ m/HP-UX/i) ||
     system("rm -f $targetdir/samples/StdInParse/Makefile");
     system("cp -Rf $XERCESCROOT/samples/EnumVal/* $targetdir/samples/EnumVal");
     system("rm -f $targetdir/samples/EnumVal/Makefile");
+    if ($platform = "solaris") {
+    	system("rm -Rf $targetdir/samples/EnumVal/Templates.DB");
+    }
     system("cp -Rf $XERCESCROOT/samples/CreateDOMDocument/* $targetdir/samples/CreateDOMDocument");
     system("rm -f $targetdir/samples/CreateDOMDocument/Makefile");
     system("rm -f $targetdir/samples/Makefile");
