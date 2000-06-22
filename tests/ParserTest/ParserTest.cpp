95610d019d1d9a08fc83fd1f70c75d7ac2184ee6
Unknown (roddey) <dev-null@apache.org>
2000-06-22 04:20:59 +0000

    'Do nothing' comment change to test my ability to commit from home.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@172205 13f79535-47bb-0310-9956-ffa450edef68

--- tests/ParserTest/ParserTest.cpp
+++ tests/ParserTest/ParserTest.cpp
@@ -1,37 +1,37 @@
 /*
  * The Apache Software License, Version 1.1
- * 
+ *
  * Copyright (c) 1999-2000 The Apache Software Foundation.  All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- * 
+ *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache\@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache",
  *    nor may "Apache" appear in their name, without prior written
  *    permission of the Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,7 +45,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the Apache Software Foundation, and was
  * originally based on software copyright (c) 1999, International
@@ -56,6 +56,10 @@
 
 /*
  * $Log$
+ * Revision 1.8  2000/06/22 04:20:59  roddey
+ * 'Do nothing' comment change to test my ability to commit
+ * from home.
+ *
  * Revision 1.7  2000/03/02 19:55:46  roddey
  * This checkin includes many changes done while waiting for the
  * 1.1.0 code to be finished. I can't list them all here, but a list is
@@ -114,8 +118,8 @@ int main(int argC, char** argV)
     }
 
     //
-    //  Create our test parser object. This guy implements the internal event
-    //  APIs and is plugged into the scanner.
+    //  Create our test parser object. This object implements the internal
+    //  event APIs and is plugged into the scanner.
     //
     TestParser parserTest;
 
@@ -244,7 +248,7 @@ void StrX::transcode(const XMLCh* const toTranscode, const unsigned int len)
     }
 
     //
-    //  If either the passed length was non-zero or our char sizes are not 
+    //  If either the passed length was non-zero or our char sizes are not
     //  same, we have to use a temp buffer. Since this is common in these
     //  samples, we just do it anyway.
     //
