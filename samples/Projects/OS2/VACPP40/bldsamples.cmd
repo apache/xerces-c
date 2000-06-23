d18303f7a4dc6c655dc16d9ea0775f40d0d6b19b
Unknown (aruna1) <dev-null@apache.org>
2000-06-23 18:12:07 +0000

    OS/2 related addition. Scripts to help building the samples in the parser
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@172210 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/Projects/OS2/VACPP40/bldsamples.cmd
@@ -0,0 +1,21 @@
+@echo off
+echo making CreateDOMDocument
+vacbld CreateDOMDocument.icc
+echo making DOMCount
+vacbld DOMCount.icc
+echo making DOMPrint
+vacbld DOMPrint.icc
+echo making EnumVal
+vacbld EnumVal.icc
+echo making MemParse
+vacbld MemParse.icc
+echo making PParse
+vacbld PParse.icc
+echo making Redirect
+vacbld Redirect.icc
+echo making SAXCount
+vacbld SAXCount.icc
+echo making SAXPrint
+vacbld SAXPrint.icc
+echo making StdInParse
+vacbld StdInParse.icc
