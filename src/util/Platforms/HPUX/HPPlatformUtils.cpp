71bf99d640978a7b4c11c0a47f264707a6ac93ee
Andy Heninger <andyh@apache.org>
2000-09-22 18:31:41 +0000

    HPUX PlatformUtils - Removed incorrect use of ArrayJanitor on a local array. Patch submitted by "Torbj�rn B�ckstr�m" <torbjorn.backstrom@teleteknik.goteborg.se>
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@172409 13f79535-47bb-0310-9956-ffa450edef68

--- src/util/Platforms/HPUX/HPPlatformUtils.cpp
+++ src/util/Platforms/HPUX/HPPlatformUtils.cpp
@@ -1,37 +1,37 @@
 /*
  * The Apache Software License, Version 1.1
- * 
+ *
  * Copyright (c) 1999-2000 The Apache Software Foundation.  All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- * 
+ *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache\@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache",
  *    nor may "Apache" appear in their name, without prior written
  *    permission of the Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,7 +45,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the Apache Software Foundation, and was
  * originally based on software copyright (c) 1999, International
@@ -56,6 +56,10 @@
 
 /*
  * $Log$
+ * Revision 1.15  2000/09/22 18:31:41  andyh
+ * HPUX PlatformUtils - Removed incorrect use of ArrayJanitor on a local array.
+ * Patch submitted by "Torbjörn Bäckström" <torbjorn.backstrom@teleteknik.goteborg.se>
+ *
  * Revision 1.14  2000/07/25 22:29:36  aruna1
  * Char definitions in XMLUni moved to XMLUniDefs
  *
@@ -413,7 +417,7 @@ void XMLPlatformUtils::resetFile(FileHandle theFile)
 // ---------------------------------------------------------------------------
 //  XMLPlatformUtils: Timing Methods
 // ---------------------------------------------------------------------------
- 
+
 unsigned long XMLPlatformUtils::getCurrentMillis()
 {
 #if defined(XML_HPUX_KAICC)         // should be reimplemented by someone with
@@ -444,7 +448,6 @@ XMLCh* XMLPlatformUtils::getFullPath(const XMLCh* const srcPath)
     char absPath[PATH_MAX];
     //get the absolute path
     char* retPath = realpath(newSrc, &absPath[0]);
-    ArrayJanitor<char> janText2(retPath);
 
     if (!retPath)
     {
@@ -581,7 +584,7 @@ XMLCh* XMLPlatformUtils::weavePaths
 }
 
 // -----------------------------------------------------------------------
-//  Mutex methods 
+//  Mutex methods
 // -----------------------------------------------------------------------
 
 #if !defined(APP_NO_THREADS)
@@ -671,8 +674,8 @@ void XMLPlatformUtils::unlockMutex(void* const mtxHandle)
 //  Miscellaneous synchronization methods
 // -----------------------------------------------------------------------
 
-void* XMLPlatformUtils::compareAndSwap ( void**      toFill, 
-                                   const void* const newValue, 
+void* XMLPlatformUtils::compareAndSwap ( void**      toFill,
+                                   const void* const newValue,
                                    const void* const toCompare)
 {
     XMLMutexLock  localLock(&atomicOpsMutex);
