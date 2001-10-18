b21ef34c0f22da272999fed478b42548a71ad80c
Tinny Ng <tng@apache.org>
2001-10-18 20:51:10 +0000

    [Bug 3666] Win32MsgLoader unable to retrieve error text if DLL is renamed.  Patch from Jerry Carter.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@173131 13f79535-47bb-0310-9956-ffa450edef68

--- src/util/MsgLoaders/Win32/Win32MsgLoader.cpp
+++ src/util/MsgLoaders/Win32/Win32MsgLoader.cpp
@@ -1,37 +1,37 @@
 /*
  * The Apache Software License, Version 1.1
- * 
- * Copyright (c) 1999-2000 The Apache Software Foundation.  All rights
+ *
+ * Copyright (c) 1999-2001 The Apache Software Foundation.  All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- * 
+ *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache\@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache",
  *    nor may "Apache" appear in their name, without prior written
  *    permission of the Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,7 +45,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the Apache Software Foundation, and was
  * originally based on software copyright (c) 1999, International
@@ -70,9 +70,32 @@
 #include <windows.h>
 #include "Win32MsgLoader.hpp"
 
-
 // ---------------------------------------------------------------------------
 //  Public Constructors and Destructor
+// ---------------------------------------------------------------------------
+HINSTANCE globalModuleHandle;
+
+BOOL APIENTRY DllMain(HINSTANCE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved)
+{
+  switch (ul_reason_for_call) {
+  case DLL_PROCESS_ATTACH:
+    globalModuleHandle = hModule;
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  }
+  return TRUE;
+}
+
+
+// ---------------------------------------------------------------------------
+//  Global module handle
 // ---------------------------------------------------------------------------
 Win32MsgLoader::Win32MsgLoader(const XMLCh* const msgDomain) :
 
@@ -81,7 +104,7 @@ Win32MsgLoader::Win32MsgLoader(const XMLCh* const msgDomain) :
     , fMsgDomain(0)
 {
     // Try to get the module handle
-    fModHandle = ::GetModuleHandleA(Xerces_DLLName);
+    fModHandle = globalModuleHandle;
     if (!fModHandle)
     {
         //
