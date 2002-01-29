eec15bad479ad85e02dc8cbf7ab9177273cdc7bd
Tinny Ng <tng@apache.org>
2002-01-29 21:16:53 +0000

    Remove those jar files that are not clear in license issue.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@173438 13f79535-47bb-0310-9956-ffa450edef68

--- createdocs.bat
+++ createdocs.bat
@@ -1,2 +1,1 @@
 java -classpath "tools/jars/stylebook-1.0-b2.jar;tools/jars/xalan.jar;tools/jars/xerces.jar" org.apache.stylebook.StyleBook "targetDirectory=doc/html" doc/xerces-c_book.xml tools/jars/style-apachexml.jar
-java -classpath "tools/jars/createPDF.jar;tools/jars/fop.jar;tools/jars/w3c.jar;tools/jars/xalan.jar;tools/jars/xerces.jar" org.apache.stylebook.pdf.CreatePDF -propfile doc/pdf/xerces-c-pdf.properties
