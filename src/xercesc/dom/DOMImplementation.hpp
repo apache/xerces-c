d40756cb7414f577ec8aabb3a989842e628ccabb
Tinny Ng <tng@apache.org>
2002-06-03 19:16:58 +0000

    Update API documentation for DOMImplementation.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@173782 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/dom/DOMImplementation.hpp
+++ src/xercesc/dom/DOMImplementation.hpp
@@ -67,51 +67,160 @@ class DOMDocument;
 class DOMDocumentType;
 
 /**
- *   This class provides a way to query the capabilities of an implementation
- *   of the DOM
+ * The <code>DOMImplementation</code> interface provides a number of methods
+ * for performing operations that are independent of any particular instance
+ * of the document object model.
  */
-
 
 class CDOM_EXPORT DOMImplementation : public DOMImplementationLS
 {
- protected:
-     DOMImplementation() {};                                      // no plain constructor
-     DOMImplementation(const DOMImplementation &other) {};   // no copy construtor.
-     DOMImplementation & operator = (const DOMImplementation &other) {return *this;};  // No Assignment
+
+protected :
+    // -----------------------------------------------------------------------
+    //  Hidden constructors
+    // -----------------------------------------------------------------------
+    /** @name Constructors */
+    //@{
+
+        DOMImplementation() {};                                      // no plain constructor
+        DOMImplementation(const DOMImplementation &other) {};   // no copy construtor.
+        DOMImplementation & operator = (const DOMImplementation &other) {return *this;};  // No Assignment
+    //@}
 
 
- public:
+public:
+    // -----------------------------------------------------------------------
+    //  All constructors are hidden, just the destructor is available
+    // -----------------------------------------------------------------------
+    /** @name Destructor */
+    //@{
+    /**
+     * Destructor
+     *
+     */
+    virtual ~DOMImplementation() {};
+    //@}
 
-     //  Factory method for getting a DOMImplementation object.
-     //     The DOM implementation retains ownership of the returned object.
-     //     Application code should NOT delete it.
-     //
- static DOMImplementation *getImplementation();
+    // -----------------------------------------------------------------------
+    // DOMImplementation interface
+    // -----------------------------------------------------------------------
+    /** @name Virtual DOMImplementation interface */
+    //@{
+    /**
+     * Test if the DOM implementation implements a specific feature.
+     * @param feature The name of the feature to test (case-insensitive). The
+     *   values used by DOM features are defined throughout the DOM Level 2
+     *   specifications and listed in the  section. The name must be an XML
+     *   name. To avoid possible conflicts, as a convention, names referring
+     *   to features defined outside the DOM specification should be made
+     *   unique.
+     * @param version This is the version number of the feature to test. In
+     *   Level 2, the string can be either "2.0" or "1.0". If the version is
+     *   not specified, supporting any version of the feature causes the
+     *   method to return <code>true</code>.
+     * @return <code>true</code> if the feature is implemented in the
+     *   specified version, <code>false</code> otherwise.
+     */
+    virtual bool  hasFeature(const XMLCh *feature,  const XMLCh *version) = 0;
 
 
+    /**
+     * Introduced in DOM Level 2
+     *
+     * Creates an empty <code>DocumentType</code> node. Entity declarations
+     * and notations are not made available. Entity reference expansions and
+     * default attribute additions do not occur. It is expected that a
+     * future version of the DOM will provide a way for populating a
+     * <code>DocumentType</code>.
+     * @param qualifiedName The qualified name of the document type to be
+     *   created.
+     * @param publicId The external subset public identifier.
+     * @param systemId The external subset system identifier.
+     * @return A new <code>DocumentType</code> node with
+     *   <code>Node.ownerDocument</code> set to <code>null</code>.
+     * @exception DOMException
+     *   INVALID_CHARACTER_ERR: Raised if the specified qualified name
+     *   contains an illegal character.
+     *   <br>NAMESPACE_ERR: Raised if the <code>qualifiedName</code> is
+     *   malformed.
+     *   <br>NOT_SUPPORTED_ERR: May be raised by DOM implementations which do
+     *   not support the <code>"XML"</code> feature, if they choose not to
+     *   support this method. Other features introduced in the future, by
+     *   the DOM WG or in extensions defined by other groups, may also
+     *   demand support for this method; please consult the definition of
+     *   the feature to see if it requires this method.
+     * @since DOM Level 2
+     */
+    virtual  DOMDocumentType *createDocumentType(const XMLCh *qualifiedName,
+                                                 const XMLCh *publicId,
+                                                 const XMLCh *systemId) = 0;
 
- virtual ~DOMImplementation() {};
+    /**
+     * Introduced in DOM Level 2
+     *
+     * Creates a DOM Document object of the specified type with its document
+     * element.
+     * @param namespaceURI The namespace URI of the document element to
+     *   create.
+     * @param qualifiedName The qualified name of the document element to be
+     *   created.
+     * @param doctype The type of document to be created or <code>null</code>.
+     *   When <code>doctype</code> is not <code>null</code>, its
+     *   <code>Node.ownerDocument</code> attribute is set to the document
+     *   being created.
+     * @return A new <code>Document</code> object.
+     * @exception DOMException
+     *   INVALID_CHARACTER_ERR: Raised if the specified qualified name
+     *   contains an illegal character.
+     *   <br>NAMESPACE_ERR: Raised if the <code>qualifiedName</code> is
+     *   malformed, if the <code>qualifiedName</code> has a prefix and the
+     *   <code>namespaceURI</code> is <code>null</code>, or if the
+     *   <code>qualifiedName</code> has a prefix that is "xml" and the
+     *   <code>namespaceURI</code> is different from "
+     *   http://www.w3.org/XML/1998/namespace" , or if the DOM
+     *   implementation does not support the <code>"XML"</code> feature but
+     *   a non-null namespace URI was provided, since namespaces were
+     *   defined by XML.
+     *   <br>WRONG_DOCUMENT_ERR: Raised if <code>doctype</code> has already
+     *   been used with a different document or was created from a different
+     *   implementation.
+     *   <br>NOT_SUPPORTED_ERR: May be raised by DOM implementations which do
+     *   not support the "XML" feature, if they choose not to support this
+     *   method. Other features introduced in the future, by the DOM WG or
+     *   in extensions defined by other groups, may also demand support for
+     *   this method; please consult the definition of the feature to see if
+     *   it requires this method.
+     * @since DOM Level 2
+     */
 
-virtual bool  hasFeature(const XMLCh *feature,  const XMLCh *version) = 0;
+    virtual DOMDocument *createDocument(const XMLCh *namespaceURI,
+                                        const XMLCh *qualifiedName,
+                                        DOMDocumentType *doctype) = 0;
 
+    //@}
 
+    // -----------------------------------------------------------------------
+    // Non-standard extension
+    // -----------------------------------------------------------------------
+    /** @name Non-standard extension */
+    //@{
+    /**
+     * Non-standard extension
+     *
+     *  Factory method for getting a DOMImplementation object.
+     *     The DOM implementation retains ownership of the returned object.
+     *     Application code should NOT delete it.
+     */
+    static DOMImplementation *getImplementation();
 
-//  Create a new DocumentType.
-//     Initially the application owns the returned DocumentType object and is responsible
-//        for deleting it.  If the DocumentType is subsequently associated with a Document,
-//        that document becomes the owner of the storage and will delete the document type
-//        when the document is deleted.
-virtual  DOMDocumentType *createDocumentType(const XMLCh *qualifiedName,
-                                               const XMLCh *publicId, const XMLCh *systemId) = 0;
+    /**
+     * Non-standard extension
+     *
+     * Create a completely empty document that has neither a root element or a doctype node.
+     */
+    virtual DOMDocument *createDocument() = 0;
 
-
-virtual DOMDocument *createDocument(const XMLCh *namespaceURI,
-                              const XMLCh *qualifiedName, DOMDocumentType *doctype) = 0;
-
-// Non-standard extension.  Create a completely empty document that has neither a root
-//          element or a doctype node.
-//
-virtual DOMDocument *createDocument() = 0;
+    //@}
 };
 
 #endif
