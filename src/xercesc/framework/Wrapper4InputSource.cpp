3f80d8646dd9539a41a171ac1205849badc2e66e
Khaled Noaman <knoaman@apache.org>
2002-06-18 19:07:07 +0000

    Initial checkin.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@173863 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/xercesc/framework/Wrapper4InputSource.cpp
@@ -0,0 +1,150 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xerces" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache\@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation, and was
+ * originally based on software copyright (c) 1999, International
+ * Business Machines, Inc., http://www.ibm.com .  For more information
+ * on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+/*
+ * $Log$
+ * Revision 1.1  2002/06/18 19:07:07  knoaman
+ * Initial checkin.
+ *
+ */
+
+
+// ---------------------------------------------------------------------------
+//  Includes
+// ---------------------------------------------------------------------------
+#include <xercesc/framework/Wrapper4InputSource.hpp>
+#include <xercesc/sax/InputSource.hpp>
+#include <xercesc/util/NullPointerException.hpp>
+
+
+// ---------------------------------------------------------------------------
+//  Wrapper4InputSource: Constructor and Destructor
+// ---------------------------------------------------------------------------
+Wrapper4InputSource::Wrapper4InputSource(InputSource* const inputSource) :
+    fAdoptInputSource(true)
+    ,  fInputSource(inputSource)
+{
+    if (!inputSource)
+        ThrowXML(NullPointerException, XMLExcepts::CPtr_PointerIsZero);
+}
+
+Wrapper4InputSource::~Wrapper4InputSource()
+{
+    if (fAdoptInputSource)
+        delete fInputSource;
+}
+
+
+// ---------------------------------------------------------------------------
+//  Wrapper4InputSource: Getter methods
+// ---------------------------------------------------------------------------
+const bool Wrapper4InputSource::getIssueFatalErrorIfNotFound() const
+{
+    return fInputSource->getIssueFatalErrorIfNotFound();
+}
+
+const XMLCh* Wrapper4InputSource::getEncoding() const
+{
+    return fInputSource->getEncoding();
+}
+
+const XMLCh* Wrapper4InputSource::getSystemId() const
+{
+    return fInputSource->getSystemId();
+}
+
+const XMLCh* Wrapper4InputSource::getPublicId() const
+{
+    return fInputSource->getPublicId();
+}
+
+
+// ---------------------------------------------------------------------------
+//  Wrapper4InputSource: Setter methods
+// ---------------------------------------------------------------------------
+void Wrapper4InputSource::setIssueFatalErrorIfNotFound(const bool flag)
+{
+    fInputSource->setIssueFatalErrorIfNotFound(flag);
+}
+
+
+void Wrapper4InputSource::setEncoding(const XMLCh* const encodingStr)
+{
+    fInputSource->setEncoding(encodingStr);
+}
+
+
+void Wrapper4InputSource::setPublicId(const XMLCh* const publicId)
+{
+    fInputSource->setPublicId(publicId);
+}
+
+
+void Wrapper4InputSource::setSystemId(const XMLCh* const systemId)
+{
+    fInputSource->setSystemId(systemId);
+}
+
+
+// ---------------------------------------------------------------------------
+//  Wrapper4InputSource: Stream methods
+// ---------------------------------------------------------------------------
+BinInputStream* Wrapper4InputSource::makeStream() const
+{
+    return fInputSource->makeStream();
+}
+
+
