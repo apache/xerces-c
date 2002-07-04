41275e48a09ab42a9887161bbd63d9584493375c
Tinny Ng <tng@apache.org>
2002-07-04 15:36:15 +0000

    should not ship outdated OS2 Project files in samples
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@173943 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/packageSources.pl
+++ scripts/packageSources.pl
@@ -154,6 +154,7 @@ sub package_sources {
    system("$RM -rf $srctargetdir/include");
    system("$RM -rf $srctargetdir/doc/*.zip");
 
+   system("$RM -rf $srctargetdir/samples/Projects/OS2");
    system("$RM -rf $srctargetdir/Projects/OS2");
    system("$RM -rf $srctargetdir/Projects/Win32/BCB4");
    system("$RM -rf $srctargetdir/Projects/Win32/BCB5");
