ea44650ae556a60df9d738a7d5d25259a02d6f3b
Tinny Ng <tng@apache.org>
2002-07-16 12:27:12 +0000

    [Bug 10648] DOMDocumentImpl misaligned allocations on machines with a 64 bits 'long' type.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@174009 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/dom/impl/DOMDocumentImpl.cpp
+++ src/xercesc/dom/impl/DOMDocumentImpl.cpp
@@ -714,7 +714,10 @@ static const int kMaxSubAllocationSize = 4096;   // Any request for more bytes
 void *         DOMDocumentImpl::allocate(size_t amount)
 {
 
-     size_t sizeOfPointer = sizeof(void *);
+//     size_t sizeOfPointer = sizeof(void *);
+// some MIPS or IA64 machines may misallign if the class has a long data type member
+// see Bug 10648 for details
+     size_t sizeOfPointer = sizeof(long);
      if (amount%sizeOfPointer!=0)
        amount = amount + (sizeOfPointer - (amount % sizeOfPointer));
 
