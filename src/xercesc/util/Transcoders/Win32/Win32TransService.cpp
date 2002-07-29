aec071e83017e358443a1460ee7536af751450f6
Tinny Ng <tng@apache.org>
2002-07-29 14:22:20 +0000

    [Bug 9533] Win32TransService does not recognize aliases for encodings.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@174075 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/util/Transcoders/Win32/Win32TransService.cpp
+++ src/xercesc/util/Transcoders/Win32/Win32TransService.cpp
@@ -1,37 +1,37 @@
 /*
  * The Apache Software License, Version 1.1
- * 
+ *
  * Copyright (c) 1999-2000 The Apache Software Foundation.  All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- * 
+ *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache\@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache",
  *    nor may "Apache" appear in their name, without prior written
  *    permission of the Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,7 +45,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the Apache Software Foundation, and was
  * originally based on software copyright (c) 1999, International
@@ -359,7 +359,6 @@ Win32TransService::Win32TransService()
     //  values we stored for the original.
     //
     subIndex = 0;
-    char aliasBuf[nameBufSz + 1];
     while (true)
     {
         // Get the name of the next key
@@ -393,12 +392,12 @@ Win32TransService::Win32TransService()
         //  then construct a new one with the new name and the aliased
         //  ids.
         //
-        if (isAlias(encodingKey, aliasBuf, nameBufSz))
+        if (isAlias(encodingKey, nameBuf, nameBufSz))
         {
-            const unsigned int srcLen = strlen(aliasBuf);
-            const unsigned int targetLen = ::mbstowcs(0, aliasBuf, srcLen);
+            const unsigned int srcLen = strlen(nameBuf);
+            const unsigned int targetLen = ::mbstowcs(0, nameBuf, srcLen);
             XMLCh* uniAlias = new XMLCh[targetLen + 1];
-            ::mbstowcs(uniAlias, aliasBuf, srcLen);
+            ::mbstowcs(uniAlias, nameBuf, srcLen);
             uniAlias[targetLen] = 0;
             _wcsupr(uniAlias);
 
@@ -495,7 +494,7 @@ void Win32TransService::lowerCase(XMLCh* const toLowerCase) const
 }
 
 bool Win32TransService::isAlias(const   HKEY            encodingKey
-                    ,       char* const     aliasBuf 
+                    ,       char* const     aliasBuf
                     , const unsigned int    nameBufSz )
 {
     unsigned long theType;
@@ -644,7 +643,7 @@ Win32Transcoder::transcodeFrom( const   XMLByte* const      srcData
                     , getEncodingName()
                 );
             }
-             else            
+             else
             {
                 ThrowXML(TranscodingException, XMLExcepts::Trans_BadSrcSeq);
             }
