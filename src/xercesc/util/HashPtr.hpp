aa9a50e3ef461a0d6c75667a22432ae5aa4cf99d
Tinny Ng <tng@apache.org>
2002-09-27 12:47:11 +0000

    [Bug 12847] bulid warning for non-virtual constuctor.  By Gareth Reakes.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@174250 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/util/HashPtr.hpp
+++ src/xercesc/util/HashPtr.hpp
@@ -1,37 +1,37 @@
 /*
  * The Apache Software License, Version 1.1
- * 
+ *
  * Copyright (c) 1999-2000 The Apache Software Foundation.  All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- * 
+ *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache\@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache",
  *    nor may "Apache" appear in their name, without prior written
  *    permission of the Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,7 +45,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the Apache Software Foundation, and was
  * originally based on software copyright (c) 1999, International
@@ -63,8 +63,8 @@
 /**
  * The <code>HashPtr</code> class inherits from <code>HashBase</code>.
  * This is a generic hasher class designed to hash the pointers of the
- * objects themselves.  Useful if you want to hash objects instead of 
- * strings so long as the objects don't move to a different memory location.  
+ * objects themselves.  Useful if you want to hash objects instead of
+ * strings so long as the objects don't move to a different memory location.
  * See <code>HashBase</code> for more information.
  */
 
@@ -72,7 +72,7 @@ class XMLUTIL_EXPORT HashPtr : public HashBase
 {
 public:
 	HashPtr();
-	~HashPtr();
+	virtual ~HashPtr();
 	virtual unsigned int getHashVal(const void *const key, unsigned int mod);
 	virtual bool equals(const void *const key1, const void *const key2);
 
