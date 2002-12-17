33b924e5c4e1100b0b11321f69fd879868a907eb
Tinny Ng <tng@apache.org>
2002-12-17 17:39:09 +0000

    Xlat: generate icu message file with array format as default.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@174534 13f79535-47bb-0310-9956-ffa450edef68

--- tools/NLS/Xlat/Xlat_ICUResourceBundle.cpp
+++ tools/NLS/Xlat/Xlat_ICUResourceBundle.cpp
@@ -1,37 +1,37 @@
 /*
  * The Apache Software License, Version 1.1
- * 
+ *
  * Copyright (c) 1999-2002 The Apache Software Foundation.  All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- * 
+ *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "Xerces" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache\@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache",
  *    nor may "Apache" appear in their name, without prior written
  *    permission of the Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,7 +45,7 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the Apache Software Foundation, and was
  * originally based on software copyright (c) 1999, International
@@ -57,6 +57,9 @@
 /*
  * $Id$
  * $Log$
+ * Revision 1.4  2002/12/17 17:39:09  tng
+ * Xlat: generate icu message file with array format as default.
+ *
  * Revision 1.3  2002/12/12 23:40:38  peiyongz
  * normlize locale string.
  *
@@ -113,8 +116,8 @@ void ICUResBundFormatter::endDomain(const   XMLCh*        const    domainName
 
 void ICUResBundFormatter::endMsgType(const MsgTypes type)
 {
-#ifdef ICU_RESBUNDLE_IN_ARRAY_FORM
-     fwprintf(fOutFl, L"\t\t\"%S End \" ,\n", typePrefixes[type]);  
+#ifndef ICU_RESBUNDLE_IN_TABLE_FORM
+     fwprintf(fOutFl, L"\t\t\"%S End \" ,\n", typePrefixes[type]);
 #endif
 }
 
@@ -141,7 +144,7 @@ ICUResBundFormatter::nextMessage(const  XMLCh* const            msgText
 
 	char* outData = fTranscoder->transcode(msgText);
 
-    // 
+    //
 	//  array_name {
 	//               "xxx" ,
 	//               "yyy" ,
@@ -154,12 +157,12 @@ ICUResBundFormatter::nextMessage(const  XMLCh* const            msgText
 	//  }
 	//
 
-#ifdef ICU_RESBUNDLE_IN_ARRAY_FORM
-    fwprintf(fOutFl, L"\t\t\"%S \" ,\n", outData);  
-	// need a space between the last character and the closing "
-#else
+#ifdef ICU_RESBUNDLE_IN_TABLE_FORM
     fwprintf(fOutFl, L"\t\t %3d { \"%S \" } \n", curId, outData);
 	// need to print leading 0 if less than 100, not tested yet
+#else
+    fwprintf(fOutFl, L"\t\t\"%S \" ,\n", outData);
+	// need a space between the last character and the closing "
 #endif
 
     delete [] outData;
@@ -177,10 +180,10 @@ void ICUResBundFormatter::startDomain(const   XMLCh* const    domainName
 	int index = XMLString::lastIndexOf(domainName, chForwardSlash);
 	char *tmpDomainName = fTranscoder->transcode(&(domainName[index+1]));
 
-#ifdef ICU_RESBUNDLE_IN_ARRAY_FORM
-    fwprintf(fOutFl, L"\n\t // an array \n");
-#else
+#ifdef ICU_RESBUNDLE_IN_TABLE_FORM
     fwprintf(fOutFl, L"\n\t // a table \n");
+#else
+    fwprintf(fOutFl, L"\n\t // an array \n");
 #endif
 
     fwprintf(fOutFl, L"\t%S { \n" , tmpDomainName);
@@ -192,8 +195,8 @@ void ICUResBundFormatter::startDomain(const   XMLCh* const    domainName
 
 void ICUResBundFormatter::startMsgType(const MsgTypes type)
 {
-#ifdef ICU_RESBUNDLE_IN_ARRAY_FORM
-    fwprintf(fOutFl, L"\t\t\"%S Start \" , \n", typePrefixes[type]);  
+#ifndef ICU_RESBUNDLE_IN_TABLE_FORM
+    fwprintf(fOutFl, L"\t\t\"%S Start \" , \n", typePrefixes[type]);
 #endif
 }
 
