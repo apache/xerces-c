5a15e29d7af3314607ac45d07c76c153d6e85304
Tinny Ng <tng@apache.org>
2003-02-05 18:56:57 +0000

    Fix doxygen warning.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@174748 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/dom/DOMElement.hpp
+++ src/xercesc/dom/DOMElement.hpp
@@ -414,6 +414,7 @@ public:
      */
     virtual bool         hasAttributeNS(const XMLCh *namespaceURI,
                                         const XMLCh *localName) const = 0;
+    //@}
 
     /** @name Functions introduced in DOM Level 3 */
     //@{
@@ -422,14 +423,14 @@ public:
      * Declares the <code>DOMAttr</code> specified by name to be of type ID. If the
      * value of the specified <code>DOMAttr</code> is unique then this element node
      * can later be retrieved using getElementById on Document. Note, however,
-     * that this simply affects this node and does not change any grammar that 
+     * that this simply affects this node and does not change any grammar that
      * may be in use.
-     * To specify an <code>DOMAttr</code> by local name and namespace URI, use the 
+     * To specify an <code>DOMAttr</code> by local name and namespace URI, use the
      * setIdAttributeNS method.
      * @param name The name of the <code>DOMAttr</code>.
      * @exception DOMException
      *    NO_MODIFICATION_ALLOWED_ERR: Raised if this node is readonly.
-     *    <br />NOT_FOUND_ERR: Raised if the specified node is not an <code>DOMAttr</code> 
+     *    <br />NOT_FOUND_ERR: Raised if the specified node is not an <code>DOMAttr</code>
      * of this element.
      *
      * <p><b>"Experimental - subject to change"</b></p>
@@ -440,11 +441,11 @@ public:
 
 
     /**
-     * Declares the <code>DOMAttr</code> specified by local name and namespace 
+     * Declares the <code>DOMAttr</code> specified by local name and namespace
      * URI to be of type ID. If the value of the specified <code>DOMAttr</code>
      * is unique then this <code>DOMElement</code> node can later be retrieved
-     * using getElementById on <code>DOMDocument</code>. Note, however, that 
-     * this simply affects this node and does not change any grammar that may 
+     * using getElementById on <code>DOMDocument</code>. Note, however, that
+     * this simply affects this node and does not change any grammar that may
      * be in use.
      * @param namespaceURI The namespace URI of the <code>DOMAttr</code>.
      * @param localName The local name of the <code>DOMAttr</code>.
@@ -463,7 +464,7 @@ public:
     /**
      * Declares the <code>DOMAttr</code> specified by node to be of type ID.
      * If the value of the specified <code>DOMAttr</code> is unique then this
-     * <code>DOMElement</code> node can later be retrieved using getElementById 
+     * <code>DOMElement</code> node can later be retrieved using getElementById
      * on <code>DOMDocument</code>. Note, however, that this simply affects this
      * node and does not change any grammar that may be in use.
      * @param idAttr The <code>DOMAttr</code> node.
@@ -473,7 +474,7 @@ public:
      *
      * <p><b>"Experimental - subject to change"</b></p>
      *
-     * @since DOM Level 3     
+     * @since DOM Level 3
      */
     virtual void setIdAttributeNode(const DOMAttr *idAttr) = 0;
 
@@ -487,7 +488,7 @@ public:
      * @return the <code>DOMTypeInfo</code> associated with this element
      * @since DOM level 3
      */
-    virtual const DOMTypeInfo* getTypeInfo() const = 0; 
+    virtual const DOMTypeInfo* getTypeInfo() const = 0;
 
     //@}
 
@@ -498,4 +499,4 @@ XERCES_CPP_NAMESPACE_END
 #endif
 
 
-    
+
