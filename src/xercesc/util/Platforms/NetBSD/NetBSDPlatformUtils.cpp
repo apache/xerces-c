50099a52cbbbd79ab0e552b264043d25eb35f9ee
Khaled Noaman <knoaman@apache.org>
2003-06-02 14:21:05 +0000

    [Bug 20413] Xerces 2.3 does not compile under NetBSD 1.6. Patch by Berin Lautenbach.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@175066 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/util/Platforms/NetBSD/NetBSDPlatformUtils.cpp
+++ src/xercesc/util/Platforms/NetBSD/NetBSDPlatformUtils.cpp
@@ -421,7 +421,8 @@ unsigned long XMLPlatformUtils::getCurrentMillis()
     return (unsigned long)(t.tv_sec*1000 + t.tv_usec);
 }
 
-XMLCh* XMLPlatformUtils::getFullPath(const XMLCh* const srcPath)
+XMLCh* XMLPlatformUtils::getFullPath(const XMLCh* const srcPath,
+                                     MemoryManager* const manager)
 {
 
     //
@@ -429,20 +430,19 @@ XMLCh* XMLPlatformUtils::getFullPath(const XMLCh* const srcPath)
     //  so we know that its not some pathological freaky path. It comes in
     //  in native format, and goes out as Unicode always
     //
-    char* newSrc = XMLString::transcode(srcPath);
-    ArrayJanitor<char> janText(newSrc);
+    char* newSrc = XMLString::transcode(srcPath, fgMemoryManager);
+    ArrayJanitor<char> janText(newSrc, fgMemoryManager);
 
     // Use a local buffer that is big enough for the largest legal path
-    char *absPath = new char[1024];
+    char absPath[1024];
     // get the absolute path 
-    char* retPath = realpath(newSrc, absPath);  
-    ArrayJanitor<char> janText2(retPath);
+    char* retPath = realpath(newSrc, absPath);
     
     if (!retPath)
     {
         ThrowXML(XMLPlatformUtilsException, XMLExcepts::File_CouldNotGetBasePathName);
     }
-    return XMLString::transcode(absPath);
+    return XMLString::transcode(absPath, manager);
 }
 
 bool XMLPlatformUtils::isRelative(const XMLCh* const toCheck)
@@ -463,7 +463,7 @@ bool XMLPlatformUtils::isRelative(const XMLCh* const toCheck)
     return true;
 }
 
-XMLCh* XMLPlatformUtils::getCurrentDirectory()
+XMLCh* XMLPlatformUtils::getCurrentDirectory(MemoryManager* const manager)
 {
     char  dirBuf[PATH_MAX + 1];
     char  *curDir = getcwd(&dirBuf[0], PATH_MAX + 1);
@@ -474,7 +474,7 @@ XMLCh* XMLPlatformUtils::getCurrentDirectory()
                  XMLExcepts::File_CouldNotGetBasePathName);
     }
 
-    return XMLString::transcode(curDir);
+    return XMLString::transcode(curDir, manager);
 }
 
 inline bool XMLPlatformUtils::isAnySlash(XMLCh c)
