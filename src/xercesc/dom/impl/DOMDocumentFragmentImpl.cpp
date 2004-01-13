b29b5b915ca929910cf9ffdd8887a045c7eb8108
Khaled Noaman <knoaman@apache.org>
2004-01-13 13:34:18 +0000

    Remove unused local static data
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@175671 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/dom/impl/DOMDocumentFragmentImpl.cpp
+++ src/xercesc/dom/impl/DOMDocumentFragmentImpl.cpp
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2001-2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2001-2004 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -67,8 +67,6 @@
 
 XERCES_CPP_NAMESPACE_BEGIN
 
-
-static const XMLCh * nam;   // Will be lazily initialized to "#document-fragment"
 
 DOMDocumentFragmentImpl::DOMDocumentFragmentImpl(DOMDocument *masterDoc)
     : fNode(masterDoc), fParent(masterDoc)
