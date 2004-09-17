4f52ba6e16be0e691982e4a07b656ad807f3757a
David Abram Cargill <cargilld@apache.org>
2004-09-17 09:58:09 +0000

    Updates to build on OS390 from Steve Dulin.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@176050 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/util/Transcoders/Uniconv390/trot.s
+++ src/xercesc/util/Transcoders/Uniconv390/trot.s
@@ -9,13 +9,16 @@
 * 
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      
+* implied. See the License for the specific language governing
+* permissions and limitations under the License.
 *
 
 *
 * $Log$
+* Revision 1.3  2004/09/17 09:58:09  cargilld
+* Updates to build on OS390 from Steve Dulin.
+*
 * Revision 1.2  2004/09/08 13:56:46  peiyongz
 * Apache License Version 2.0
 *
