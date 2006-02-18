7ecfcbef94a4956aab21006c73af1ff20f08c3a2
Alberto Massari <amassari@apache.org>
2006-02-18 15:53:31 +0000

    When serializing attribute values, escape also carriage returns and tabs (jira# 1547)
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/branches/xerces-2.7@378734 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/framework/XMLFormatter.cpp
+++ src/xercesc/framework/XMLFormatter.cpp
@@ -74,13 +74,13 @@ static const XMLCh  gQuoteRef[] =
     chAmpersand, chLatin_q, chLatin_u, chLatin_o, chLatin_t, chSemiColon, chNull
 };
 
-static const unsigned int kEscapeCount = 6;
+static const unsigned int kEscapeCount = 7;
 static const XMLCh gEscapeChars[XMLFormatter::EscapeFlags_Count][kEscapeCount] =
 {
-        { chNull      , chNull       , chNull        , chNull       , chNull        , chNull }
-    ,   { chAmpersand , chCloseAngle , chDoubleQuote , chOpenAngle  , chSingleQuote , chNull }
-    ,   { chAmpersand , chOpenAngle  , chDoubleQuote , chLF         , chNull        , chNull }
-    ,   { chAmpersand , chOpenAngle  , chCloseAngle  , chNull       , chNull        , chNull }
+        { chNull      , chNull       , chNull        , chNull       , chNull        , chNull    , chNull }
+    ,   { chAmpersand , chCloseAngle , chDoubleQuote , chOpenAngle  , chSingleQuote , chNull    , chNull }
+    ,   { chAmpersand , chOpenAngle  , chDoubleQuote , chLF         , chCR          , chHTab    , chNull }
+    ,   { chAmpersand , chOpenAngle  , chCloseAngle  , chNull       , chNull        , chNull    , chNull }
 };
 
 // ---------------------------------------------------------------------------
