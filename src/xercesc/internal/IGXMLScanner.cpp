5a333bead65c7dc592b135056cac50d8ef06ed70
Alberto Massari <amassari@apache.org>
2007-01-31 16:09:31 +0000

    Handle escaped characters in schemaLocation attributes
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@501872 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/internal/IGXMLScanner.cpp
+++ src/xercesc/internal/IGXMLScanner.cpp
@@ -3195,6 +3195,10 @@ void IGXMLScanner::processSchemaLocation(XMLCh* const schemaLoc)
     while (*locStr)
     {
         do {
+            // Do we have an escaped character ?
+            if (*locStr == 0xFFFF)
+                continue;
+
             if (!curReader->isWhitespace(*locStr))
                break;
 
@@ -3206,6 +3210,9 @@ void IGXMLScanner::processSchemaLocation(XMLCh* const schemaLoc)
             fLocationPairs->addElement(locStr);
 
             while (*++locStr) {
+                // Do we have an escaped character ?
+                if (*locStr == 0xFFFF)
+                    continue;
                 if (curReader->isWhitespace(*locStr))
                     break;
             }
