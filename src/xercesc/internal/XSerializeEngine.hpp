43ab56d3a21c35e7eb544098e549e460d60a7dec
Gareth Reakes <gareth@apache.org>
2007-08-21 08:16:24 +0000

    Add explicit type conversion to get rid of a warning. Thanks to Boris Kolpackov
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/branches/xerces-2.7@568010 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/internal/XSerializeEngine.hpp
+++ src/xercesc/internal/XSerializeEngine.hpp
@@ -558,16 +558,16 @@ public:
       *
       ***/
     inline 
-    unsigned long   getBufSize()    const; 
+    XMLSize_t   getBufSize()    const;
 
     inline 
-    unsigned long   getBufCur()     const; 
+    XMLSize_t   getBufCur()     const;
 
     inline 
-    unsigned long   getBufCurAccumulated()     const; 
+    XMLSize_t   getBufCurAccumulated()     const;
 
     inline 
-    unsigned long   getBufCount()    const; 
+    XMLSize_t   getBufCount()    const;
 
     void                  trace(char*)     const;
 
@@ -633,11 +633,11 @@ private:
                                 , const XMLExcepts::Codes toThrow)  const;
 
 
-    inline size_t          calBytesNeeded(size_t)  const;
+    inline XMLSize_t          calBytesNeeded(XMLSize_t)  const;
 
-    inline size_t          alignAdjust(size_t)     const;
+    inline XMLSize_t          alignAdjust(XMLSize_t)     const;
 
-    inline void            alignBufCur(size_t);
+    inline void            alignBufCur(XMLSize_t);
 
     // Make XTemplateSerializer friend of XSerializeEngine so that
     // we can call lookupStorePool and lookupLoadPool in the case of
@@ -699,10 +699,10 @@ private:
     BinInputStream*  const                 fInputStream;
     BinOutputStream* const                 fOutputStream;
 
-    unsigned long                          fBufCount;
+    XMLSize_t                              fBufCount;
 
     //buffer
-    const unsigned long                    fBufSize;
+    const XMLSize_t                        fBufSize;
 	XMLByte* const                         fBufStart;
 	XMLByte* const                         fBufEnd; 
     XMLByte*                               fBufCur;
@@ -807,25 +807,25 @@ inline void XSerializeEngine::readString(XMLByte*&      toRead)
 }
 
 inline 
-unsigned long XSerializeEngine::getBufSize() const
+XMLSize_t XSerializeEngine::getBufSize() const
 {
     return fBufSize;
 }
 
 inline 
-unsigned long XSerializeEngine::getBufCur() const
+XMLSize_t XSerializeEngine::getBufCur() const
 {
-    return (fBufCur-fBufStart);
+    return XMLSize_t (fBufCur-fBufStart);
 }
 
 inline 
-unsigned long XSerializeEngine::getBufCurAccumulated() const
+XMLSize_t XSerializeEngine::getBufCurAccumulated() const
 {
     return (fBufCount - (isStoring() ? 0: 1)) * fBufSize + (fBufCur-fBufStart);
 }
 
 inline 
-unsigned long XSerializeEngine::getBufCount() const
+XMLSize_t XSerializeEngine::getBufCount() const
 {
     return fBufCount;
 }
