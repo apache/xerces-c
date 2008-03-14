16054dd85ac65c4a980c08fd53217ab12e5f4907
Boris Kolpackov <borisk@apache.org>
2008-03-14 14:55:42 +0000

    Move the documentation creation scripts to tools
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@637124 13f79535-47bb-0310-9956-ffa450edef68

--- createDocs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Djava.awt.headless=true -classpath "tools/jars/stylebook-1.0-b2.jar:tools/jars/xalan.jar:tools/jars/xerces.jar" org.apache.stylebook.StyleBook "targetDirectory=doc/html" doc/xerces-c_book.xml doc/style
--- createdocs.bat
+++
@@ -1,1 +0,0 @@
-java -classpath "tools/jars/stylebook-1.0-b2.jar;tools/jars/xalan.jar;tools/jars/xerces.jar" org.apache.stylebook.StyleBook "targetDirectory=doc/html" doc/xerces-c_book.xml doc/style
