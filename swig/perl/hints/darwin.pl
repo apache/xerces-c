c275b96019b32472751330a08873e9a921dd2ec7
Jason Edward Stewart <jasons@apache.org>
2008-04-12 11:20:28 +0000

    reorganization of xerces-p back into it's own SVN repo
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@647429 13f79535-47bb-0310-9956-ffa450edef68

--- swig/perl/hints/darwin.pl
+++
@@ -1,2 +0,0 @@
-# we add libstdc++ otherwise OSX gets a bunch of undefined symbols
-$self->{LIBS}->[0] .= ' -lstdc++';
--- swig/perl/hints/linux.pl
+++
@@ -1,2 +0,0 @@
-# until I get ahold of Detect::OS, I assume all linux is debian
-$self->{CCFLAGS} .= " -DDEBIAN -D_GNU_SOURCE";
