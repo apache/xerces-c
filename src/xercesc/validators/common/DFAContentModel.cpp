eec5d27bbf1a6fac7cc44f2743997bdab171fb4b
Alberto Massari <amassari@apache.org>
2008-07-15 14:14:56 +0000

    Fixed regression due to incomplete backport for XERCESC-1051 (XERCESC-1817)
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@676924 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/validators/common/DFAContentModel.cpp
+++ src/xercesc/validators/common/DFAContentModel.cpp
@@ -207,7 +207,7 @@ DFAContentModel::validateContent( QName** const        children
                 {
                     nextState = fTransTable[curState][elemIndex];
                     if (nextState != XMLContentModel::gInvalidTrans)
-                            break;
+                        break;
                 }
                 else if ((type & 0x0f) == ContentSpecNode::Any_NS)
                 {
@@ -251,8 +251,94 @@ DFAContentModel::validateContent( QName** const        children
             if (o != 0) {
                 if (curState == nextState) {
                     if (++loopCount > (unsigned int)o->maxOccurs && o->maxOccurs != -1) {
-                        *indexFailingChild=childIndex;
-                        return false;
+
+                        // It's likely that we looped too many times on the current state
+                        // however it's possible that we actually matched another particle
+                        // which allows the same name.
+                        //
+                        // Consider:
+                        //
+                        // <xs:sequence>
+                        //  <xs:element name="foo" type="xs:string" minOccurs="3" maxOccurs="3"/>
+                        //  <xs:element name="foo" type="xs:string" fixed="bar"/>
+                        // </xs:sequence>
+                        //
+                        // and
+                        //
+                        // <xs:sequence>
+                        //  <xs:element name="foo" type="xs:string" minOccurs="3" maxOccurs="3"/>
+                        //  <xs:any namespace="##any" processContents="skip"/>
+                        // </xs:sequence>
+                        //
+                        // In the DFA there will be two transitions from the current state which 
+                        // allow "foo". Note that this is not a UPA violation. The ambiguity of which
+                        // transition to take is resolved by the current value of the counter. Since 
+                        // we've already seen enough instances of the first "foo" perhaps there is
+                        // another element declaration or wildcard deeper in the element map which
+                        // matches.
+                        unsigned int tempNextState = 0;
+                        
+                        while (++elemIndex < fElemMapSize) {
+                            const QName* inElem  = fElemMap[elemIndex];
+                            if (fDTD) {
+                                if (XMLString::equals(inElem->getRawName(), curElemRawName)) {
+                                    tempNextState = fTransTable[curState][elemIndex];
+                                    if (tempNextState != XMLContentModel::gInvalidTrans)
+                                        break;
+                                }
+                            }
+                            else {
+                                ContentSpecNode::NodeTypes type = fElemMapType[elemIndex] ;
+                                if (type == ContentSpecNode::Leaf) {
+                                    if ((inElem->getURI() == curElem->getURI()) &&
+                                    (XMLString::equals(inElem->getLocalPart(), curElem->getLocalPart()))) {
+                                        tempNextState = fTransTable[curState][elemIndex];
+                                        if (tempNextState != XMLContentModel::gInvalidTrans)
+                                            break;
+                                    }
+                                }
+                                else if ((type & 0x0f)== ContentSpecNode::Any)
+                                {
+                                    tempNextState = fTransTable[curState][elemIndex];
+                                    if (tempNextState != XMLContentModel::gInvalidTrans)
+                                        break;
+                                }
+                                else if ((type & 0x0f) == ContentSpecNode::Any_NS)
+                                {
+                                    if (inElem->getURI() == curElem->getURI())
+                                    {
+                                        tempNextState = fTransTable[curState][elemIndex];
+                                        if (tempNextState != XMLContentModel::gInvalidTrans)
+                                            break;
+                                    }
+                                }
+                                else if ((type & 0x0f) == ContentSpecNode::Any_Other)
+                                {
+                                    // Here we assume that empty string has id 1.
+                                    //
+                                    unsigned int uriId = curElem->getURI();
+                                    if (uriId != 1 && uriId != inElem->getURI()) {
+                                        tempNextState = fTransTable[curState][elemIndex];
+                                        if (tempNextState != XMLContentModel::gInvalidTrans)
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                        
+                        // if we still can't find a match, report the error
+                        if (elemIndex == fElemMapSize) {
+                            *indexFailingChild=childIndex;
+                            return false;
+                        }
+                        
+                        // if we found a match, set the next state and reset the 
+                        // counter if the next state is a counting state.
+                        nextState = tempNextState;
+                        Occurence* o = fCountingStates[nextState];
+                        if (o != 0) {
+                            loopCount = (elemIndex == o->elemIndex) ? 1 : 0;
+                        }
                     }
                 }
                 else if (loopCount < (unsigned int)o->minOccurs) {
@@ -413,8 +499,87 @@ bool DFAContentModel::validateContentSpecial(QName** const            children
             if (o != 0) {
                 if (curState == nextState) {
                     if (++loopCount > (unsigned int)o->maxOccurs && o->maxOccurs != -1) {
-                        *indexFailingChild=childIndex;
-                        return false;
+                        // It's likely that we looped too many times on the current state
+                        // however it's possible that we actually matched another particle
+                        // which allows the same name.
+                        //
+                        // Consider:
+                        //
+                        // <xs:sequence>
+                        //  <xs:element name="foo" type="xs:string" minOccurs="3" maxOccurs="3"/>
+                        //  <xs:element name="foo" type="xs:string" fixed="bar"/>
+                        // </xs:sequence>
+                        //
+                        // and
+                        //
+                        // <xs:sequence>
+                        //  <xs:element name="foo" type="xs:string" minOccurs="3" maxOccurs="3"/>
+                        //  <xs:any namespace="##any" processContents="skip"/>
+                        // </xs:sequence>
+                        //
+                        // In the DFA there will be two transitions from the current state which 
+                        // allow "foo". Note that this is not a UPA violation. The ambiguity of which
+                        // transition to take is resolved by the current value of the counter. Since 
+                        // we've already seen enough instances of the first "foo" perhaps there is
+                        // another element declaration or wildcard deeper in the element map which
+                        // matches.
+                        unsigned int tempNextState = 0;
+                        
+                        while (++elemIndex < fElemMapSize) {
+                            QName* inElem  = fElemMap[elemIndex];
+                            ContentSpecNode::NodeTypes type = fElemMapType[elemIndex];
+                            if (type == ContentSpecNode::Leaf)
+                            {
+                                if (comparator.isEquivalentTo(curElem, inElem) )
+                                {
+                                    tempNextState = fTransTable[curState][elemIndex];
+                                    if (tempNextState != XMLContentModel::gInvalidTrans)
+                                        break;
+                                }
+
+                            }
+                            else if ((type & 0x0f)== ContentSpecNode::Any)
+                            {
+                                tempNextState = fTransTable[curState][elemIndex];
+                                if (tempNextState != XMLContentModel::gInvalidTrans)
+                                    break;
+                            }
+                            else if ((type & 0x0f) == ContentSpecNode::Any_NS)
+                            {
+                                if (inElem->getURI() == curElem->getURI())
+                                {
+                                    tempNextState = fTransTable[curState][elemIndex];
+                                    if (tempNextState != XMLContentModel::gInvalidTrans)
+                                        break;
+                                }
+                            }
+                            else if ((type & 0x0f) == ContentSpecNode::Any_Other)
+                            {
+                                // Here we assume that empty string has id 1.
+                                //
+                                unsigned int uriId = curElem->getURI();
+                                if (uriId != 1 && uriId != inElem->getURI())
+                                {
+                                    tempNextState = fTransTable[curState][elemIndex];
+                                    if (tempNextState != XMLContentModel::gInvalidTrans)
+                                        break;
+                                }
+                            }
+                        }
+                        
+                        // if we still can't find a match, report the error
+                        if (elemIndex == fElemMapSize) {
+                            *indexFailingChild=childIndex;
+                            return false;
+                        }
+                        
+                        // if we found a match, set the next state and reset the 
+                        // counter if the next state is a counting state.
+                        nextState = tempNextState;
+                        Occurence* o = fCountingStates[nextState];
+                        if (o != 0) {
+                            loopCount = (elemIndex == o->elemIndex) ? 1 : 0;
+                        }
                     }
                 }
                 else if (loopCount < (unsigned int)o->minOccurs) {
