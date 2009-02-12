77d8fb21d958c0adb5cdaf067277acbd5c1582de
Boris Kolpackov <borisk@apache.org>
2009-02-12 14:10:30 +0000

    More robust handling of leading/trailing whitespaces (XERCESC-1838).
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/branches/xerces-3.0@743756 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/validators/schema/SchemaValidator.hpp
+++ src/xercesc/validators/schema/SchemaValidator.hpp
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -269,6 +269,9 @@ private:
     //  fTrailing
     //      Previous chunk had a trailing space
     //
+    //  fSeenNonWhiteSpace
+    //      Seen a non-whitespace character in the previous chunk
+    //
     //  fSeenId
     //      Indicate if an attribute of ID type has been seen already, reset per element.
     //
@@ -293,6 +296,7 @@ private:
     XMLBuffer*                      fNotationBuf;
     XMLBuffer                       fDatatypeBuffer;
     bool                            fTrailing;
+    bool                            fSeenNonWhiteSpace;
     bool                            fSeenId;
     XSDErrorReporter                fSchemaErrorReporter;
     ValueStackOf<ComplexTypeInfo*>* fTypeStack;
@@ -345,7 +349,7 @@ inline ComplexTypeInfo* SchemaValidator::getCurrentTypeInfo() const {
     return fTypeStack->peek();
 }
 
-inline DatatypeValidator * SchemaValidator::getCurrentDatatypeValidator() const 
+inline DatatypeValidator * SchemaValidator::getCurrentDatatypeValidator() const
 {
     return fCurrentDatatypeValidator;
 }
