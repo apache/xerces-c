0d3f30ffd8ea0647b3f8802851227672cf9ae404
Boris Kolpackov <borisk@apache.org>
2009-11-04 14:32:26 +0000

    Use US-ASCII instead of USASCII.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@832761 13f79535-47bb-0310-9956-ffa450edef68

--- samples/src/SAXPrint/SAXPrint.cpp
+++ samples/src/SAXPrint/SAXPrint.cpp
@@ -85,7 +85,7 @@ static void usage()
              "    -?          Show this help.\n\n"
              "  * = Default if not provided explicitly.\n\n"
              "The parser has intrinsic support for the following encodings:\n"
-             "    UTF-8, USASCII, ISO8859-1, UTF-16[BL]E, UCS-4[BL]E,\n"
+             "    UTF-8, US-ASCII, ISO8859-1, UTF-16[BL]E, UCS-4[BL]E,\n"
              "    WINDOWS-1252, IBM1140, IBM037, IBM1047.\n"
          <<  XERCES_STD_QUALIFIER endl;
 }
