b9b84208cec73bb3f6a44405bdaa13d0fd96062b
Roger Leigh <rleigh@apache.org>
2017-06-14 21:15:57 +0000

    xercesc: RangeToken: Conditionally define variable if ICU support is enabled
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@1798760 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/util/regx/RangeToken.cpp
+++ src/xercesc/util/regx/RangeToken.cpp
@@ -97,6 +97,7 @@ struct ExceptionCharsStruct
 };
 
 
+#if !(XERCES_USE_TRANSCODER_ICU && ((U_ICU_VERSION_MAJOR_NUM > 2) || (U_ICU_VERSION_MAJOR_NUM == 2 && U_ICU_VERSION_MINOR_NUM >=4)))
 // This is an array of character mappings that we will
 // add to ranges for case-insensitive matching.
 static const ExceptionCharsStruct   s_exceptions[] =
@@ -148,6 +149,7 @@ static const ExceptionCharsStruct   s_exceptions[] =
     { 0x1e60, 0x1e9b },
     { 0x1e61, 0x1e9b }
 };
+#endif
 
 // ---------------------------------------------------------------------------
 //  RangeToken: Getter methods
