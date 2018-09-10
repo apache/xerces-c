57a6cf99a336381ab90b74e52594dfe80e65c2a8
Scott Cantor <scantor@apache.org>
2018-09-10 23:11:13 +0000

    Add warning to getTextContent doc.
    git-svn-id: https://svn.apache.org/repos/asf/xerces/c/trunk@1840502 13f79535-47bb-0310-9956-ffa450edef68

--- src/xercesc/dom/DOMNode.hpp
+++ src/xercesc/dom/DOMNode.hpp
@@ -738,7 +738,10 @@ public:
     virtual short             compareDocumentPosition(const DOMNode* other) const = 0;
 
     /**
-     * This attribute returns the text content of this node and its
+     * <strong>WARNING:</strong> This method is known to be buggy and does
+     * not produce accurate results under a variety of conditions.
+     *
+     * <br>This attribute returns the text content of this node and its
      * descendants. No serialization is performed, the returned string
      * does not contain any markup. No whitespace normalization is
      * performed and the returned string does not contain the white
